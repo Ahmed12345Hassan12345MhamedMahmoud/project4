6e12480aa4988a57c28ea29da31d7b45c5ca0acf
Your Name <you@example.com>
2023-10-28 04:39:43 -0700

    project4

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// if(navigator.serviceWorker){
+
+//     navigator.serviceWorker.register("../PWA.js")
+//     .then((reg)=>{
+
+//            console.log("file registrr" , reg)
+//     })
+
+//     .catch((err)=>{
+
+//          console.log("error" ,err)
+//     })
+// }
